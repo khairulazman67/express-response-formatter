b3e385c817cfbc1c5adb7fae90385de5d06cc31d
khairulazman67 <azman.khairul67@gmail.com>
2025-06-28 02:18:05 +0700

    update package json

--- index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./src/custom.error";
-export * from "./src/dynamic-custom.error";
-export * from "./src/response.formatter";
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./custom.error";
+export * from "./dynamic-custom.error";
+export * from "./response.formatter";
